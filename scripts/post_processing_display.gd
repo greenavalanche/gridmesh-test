cf9c88c370eab23f94cf2888740b469c3d7aeaac
shadeofblue <blue@wave460.net>
2025-03-19 09:03:50 +0100

    actual selective desaturation shader poc

--- scripts/post_processing_display.gd
+++ scripts/post_processing_display.gd
@@ -3,31 +3,25 @@ extends TextureRect
 @onready var main_camera: Camera3D = $"%Viewer/Camera3D"
 @onready var grid_map: GridMap = %GridMap
 
-const EFFECT_TEXTURE_SIZE: int = 64  # Set the resolution of the effect map
+const EFFECT_TEXTURE_SIZE: int = 1024  # Set the resolution of the effect map
 var effect_texture: ImageTexture
-var counter: float = 0.0
 
 func _ready():
 	generate_effect_map()
-	var _material = material as ShaderMaterial
-	_material.set_shader_parameter("effect_map", effect_texture)
+	material.set_shader_parameter("effect_map", effect_texture)
 	print("shader initialized")
 
 func _process(_delta: float) -> void:
-	# gridmap's transform
-	material.set_shader_parameter("gridmap_transform", grid_map.global_transform)
-
-	var camera_projection = main_camera.get_camera_projection()
-	material.set_shader_parameter("camera_projection", camera_projection)
-	var camera_transform = main_camera.global_transform
-	material.set_shader_parameter("camera_transform", camera_transform)
+	material.set_shader_parameter("gridmap_transform", grid_map.global_transform.affine_inverse())
+	material.set_shader_parameter("inv_camera_projection", main_camera.get_camera_projection().inverse())
+	material.set_shader_parameter("camera_transform", main_camera.global_transform)
 
 func generate_effect_map():
 	var image = Image.create(EFFECT_TEXTURE_SIZE, EFFECT_TEXTURE_SIZE, false, Image.FORMAT_R8)
 	image.fill(Color(0, 0, 0))  # Default: No grayscale effect
 	
 	for tile in grid_map.get_used_cells():
-		var grayscale_intensity = 1.0 #get_grayscale_intensity_for_tile(tile)
+		var grayscale_intensity = get_grayscale_intensity_for_tile(tile)
 		
 		var uv_x = tile.x % EFFECT_TEXTURE_SIZE + EFFECT_TEXTURE_SIZE / 2
 		var uv_y = tile.z % EFFECT_TEXTURE_SIZE + EFFECT_TEXTURE_SIZE / 2
@@ -42,6 +36,4 @@ func get_grayscale_intensity_for_tile(tile_pos: Vector3i) -> float:
 	
 	match tile_id:
 		0: return 0.0  # No grayscale (Normal tile)
-		1: return 0.5  # Partial grayscale
-		2: return 1.0  # Full grayscale
 		_: return 1.0
