c4a17ab5ff751d6cd60f13c5c433a4d13412af4f
shadeofblue <blue@wave460.net>
2025-04-15 21:53:32 +0200

    update controls with feedback from playtesters :D

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -10,16 +10,36 @@ const TEST_PIXEL_SCENE = preload("res://scenes/test_pixel.tscn")
 @onready var viewer: RemoteTransform3D = %Viewer
 @onready var map: GridMap = %GridMap
 var grid_position: Vector3i
+var control_position: Vector3
 
 var pixel_nodes: Array[Node3D] = []
 
-func _ready() -> void:
-	update_position(true)
 
-func _process(_delta: float) -> void:
-	update_position()
+func _ready() -> void:
+	update_position(viewer.global_position, true)
+	control_position = map.map_to_local(grid_position)
+
+func _process(delta: float) -> void:
+	#update_position(viewer.global_position)
+	process_move(delta)
+	update_position(control_position)
+	if Input.is_action_just_pressed("cursor_reset"):
+		cursor_reset()
 	if Input.is_action_just_pressed("tile_action"):
 		tile_action.emit(grid_position)
+
+func process_move(delta: float):
+	if (control_position - map.map_to_local(grid_position)).length_squared() > 1.0:
+		control_position = map.map_to_local(grid_position)
+
+	var move_input = Vector2(Input.get_axis("cursor_left", "cursor_right"), Input.get_axis("cursor_up", "cursor_down"))
+	if move_input.length_squared() > viewer.INPUT_DEADZONE:
+		var position_delta = Vector3(move_input.x, 0, move_input.y) * delta * viewer.MOVE_FACTOR
+		position_delta = position_delta.rotated(Vector3.UP, viewer.rotation.y)
+		control_position += position_delta
+
+func cursor_reset():
+	control_position = map.map_to_local(game.player.grid_position)
 
 func draw_test_line(pixels: Array[Vector3i]):
 	for p in pixel_nodes:
@@ -34,10 +54,13 @@ func draw_test_line(pixels: Array[Vector3i]):
 		map.add_child(pixel_node)
 
 
-func update_position(force_emit: bool = false):
+func update_position(input_position: Vector3, force_emit: bool = false):
 	if not game.is_node_ready():
+		grid_position = map.local_to_map(input_position)
+		position = map.map_to_local(grid_position)
 		return
-	var projected_position: Vector3i = map.local_to_map(viewer.global_position)
+
+	var projected_position: Vector3i = map.local_to_map(input_position)
 	var pixels = Utils.grid_interpolate(projected_position, game.player.grid_position)
 	
 	#draw_test_line(pixels)
@@ -49,7 +72,7 @@ func update_position(force_emit: bool = false):
 			new_position = p
 			break
 
-	global_transform.origin = map.map_to_local(grid_position)
+	position = map.map_to_local(grid_position)
 	if new_position != grid_position or force_emit:
 		var old_pos_distance = projected_position.distance_squared_to(grid_position)
 		var new_pos_distance = projected_position.distance_squared_to(new_position)
